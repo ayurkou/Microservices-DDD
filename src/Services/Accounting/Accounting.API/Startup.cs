2e95588a65e4f8c2840430232a9e713dd8293662
Alexey Bogdan <alexbroyyy@gmail.com>
2018-10-17 12:30:41 +0300

    added swagger to Accounting.API

--- src/Services/Accounting/Accounting.API/Startup.cs
+++ src/Services/Accounting/Accounting.API/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace accounting.api
 {
@@ -25,7 +19,14 @@ namespace accounting.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Info {Title = "Daytona Accounting API", Version = "v1"});
+                    c.DescribeAllEnumsAsStrings();
+                });
+            services
+                .AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,12 @@ namespace accounting.api
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Daytona Accounting API V1");
+            });
             app.UseHttpsRedirection();
             app.UseMvc();
         }
