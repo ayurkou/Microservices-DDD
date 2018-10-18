98411406b2850812b410602f0f9b98720c858892
Alexey Bogdan <alexbroyyy@gmail.com>
2018-10-18 14:17:28 +0300

    fixed api versioning in Notifications.API

---
+++ src/Services/Notifications/Notifications.API/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Notifications.Api.Controllers
+{
+    [ApiVersion("0.1", Deprecated = true)]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- src/Services/Notifications/Notifications.API/Startup.cs
+++ src/Services/Notifications/Notifications.API/Startup.cs
@@ -31,59 +31,59 @@ namespace Notifications.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-//            #region API Versioning
-//            // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
-//            // note: the specified format code will format the version as "'v'major[.minor][-status]"
-//            services.AddMvcCore()
-//                .AddApiExplorer()
-//                .AddVersionedApiExplorer(
-//                options =>
-//                {
-//                    options.GroupNameFormat = "'v'VVV";
-//
-//                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
-//                    // can also be used to control the format of the API version in route templates
-//                    options.ApiVersionParameterSource = new UrlSegmentApiVersionReader();
-//                    options.AssumeDefaultVersionWhenUnspecified = true;
-//                    options.DefaultApiVersion = new ApiVersion(1, 0);
-//                    options.DefaultApiVersionParameterDescription = "default";
-//                    options.SubstituteApiVersionInUrl = true;
-//                    options.SubstitutionFormat = "VVV";
-//                });
-//
-//            services.AddApiVersioning(
-//                opts =>
-//                {
-//                    opts.ReportApiVersions = true;
-//                    opts.AssumeDefaultVersionWhenUnspecified = true;
-//                    opts.DefaultApiVersion = new ApiVersion(1, 0);
-//                });
-//
-//            #endregion API Versioning
+            #region API Versioning
+            // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+            // note: the specified format code will format the version as "'v'major[.minor][-status]"
+            services.AddMvcCore()
+                .AddApiExplorer()
+                .AddVersionedApiExplorer(
+                options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.ApiVersionParameterSource = new UrlSegmentApiVersionReader();
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.DefaultApiVersion = new ApiVersion(1, 0);
+                    options.DefaultApiVersionParameterDescription = "default";
+                    options.SubstituteApiVersionInUrl = true;
+                    options.SubstitutionFormat = "VVV";
+                });
+
+            services.AddApiVersioning(
+                opts =>
+                {
+                    opts.ReportApiVersions = true;
+                    opts.AssumeDefaultVersionWhenUnspecified = true;
+                    opts.DefaultApiVersion = new ApiVersion(1, 0);
+                });
+
+            #endregion API Versioning
 
             services
                 .AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-//            services.AddSwaggerGen(
-//                options =>
-//                {
-//                    // resolve the IApiVersionDescriptionProvider service
-//                    // note: that we have to build a temporary service provider here because one has not been created yet
-//                    var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
-//
-//                    // add a swagger document for each discovered API version
-//                    // note: you might choose to skip or document deprecated API versions differently
-//                    foreach (var description in provider.ApiVersionDescriptions)
-//                    {
-//                        options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
-//                    }
-//
-//                    // add a custom operation filter which sets default values
-//                    //options.OperationFilter<SwaggerDefaultValues>();
-//
-//                    // integrate xml comments
-//                    options.IncludeXmlComments(XmlCommentsFilePath);
-//                });       
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // resolve the IApiVersionDescriptionProvider service
+                    // note: that we have to build a temporary service provider here because one has not been created yet
+                    var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                    // add a swagger document for each discovered API version
+                    // note: you might choose to skip or document deprecated API versions differently
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                    }
+
+                    // add a custom operation filter which sets default values
+                    //options.OperationFilter<SwaggerDefaultValues>();
+
+                    // integrate xml comments
+                    options.IncludeXmlComments(XmlCommentsFilePath);
+                });       
 
             var containerBuilder = new ContainerBuilder();
             containerBuilder.RegisterModule(new RabbitMqModule(Config.BusConfig));
@@ -95,7 +95,7 @@ namespace Notifications.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -106,17 +106,16 @@ namespace Notifications.Api
                 app.UseHsts();
             }
             
-//            app.UseSwagger();
-//            app.UseSwaggerUI(c =>
-//            {
-//                c.RoutePrefix = string.Empty;
-//                
-//                // build a swagger endpoint for each discovered API version
-//                foreach (var description in provider.ApiVersionDescriptions)
-//                {
-//                    c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
-//                }
-//            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                // build a swagger endpoint for each discovered API version
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
@@ -134,7 +133,7 @@ namespace Notifications.Api
 
         static Info CreateInfoForApiVersion(ApiVersionDescription description)
         {
-            var info = new Info()
+            var info = new Info
             {
                 Title = $"Daytona Accounting API {description.ApiVersion}",
                 Version = description.ApiVersion.ToString()
